b47a4e45dd8faf04ccf7f3c124d0f5ce02da2df3
Gerardo Marin Parra <gerardo@operu.pe>
2023-07-31 22:55:01 -0500

    [MIG] sale_automatic_workflow_delivery_state: Migration to 16.0

--- sale_automatic_workflow_delivery_state/models/sale_order.py
+++ sale_automatic_workflow_delivery_state/models/sale_order.py
@@ -8,7 +8,7 @@ from odoo import api, models
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    @api.depends("delivery_state")
+    @api.depends("delivery_status")
     def _compute_all_qty_delivered(self):
         for order in self:
-            order.all_qty_delivered = order.delivery_state == "done"
+            order.all_qty_delivered = order.delivery_status == "full"
---
+++ sale_automatic_workflow_delivery_state/readme/CONTRIBUTORS.rst
@@ -0,0 +1,4 @@
+
+* `Pesol <https://www.pesol.es>`__:
+
+  * Gerardo Marin Parra <info@pesol.es>
